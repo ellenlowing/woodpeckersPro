ad1cbdac3939eca983eff788bf7f7bf05c15c8fb
Ellen Lo <lowing@bu.edu>
2017-04-18 19:05:13 -0400

    Added tests and src folder

--- src/deterWoodpeckers.ino
+++ src/deterWoodpeckers.ino
@@ -7,6 +7,7 @@
 */
 
 // defines pins numbers
+//trigPin and echoPin are the ultrasonic sensor's pins
 const int trigPin = 9; //digital
 const int echoPin = 10; //digital
 const int speakerPin = 6; //digital
@@ -21,15 +22,19 @@ const int lightThreshold = 5;
 void setup() {
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-//pinMode(speakerPin, OUTPUT);
 Serial.begin(9600); // Starts the serial communication
 }
 
+//1. Detects light level
+//2.1 If it's too dark, speaker doesn't make any sound and sensor is not triggered
+//2.2 If it's day time, we first trigger ultrasonic sensor to generate ultrasound and then we read if there's pulse
+//    If the distance detected is close enough, we play sound from file
+//3. Regardless of motion detection mechanism, we play sound for a duration of two minutes every ten minutes
+//   (only during day time)
+
 void loop() {
-// Clears the trigPin
   lightLevel = analogRead(opAmpPin);
-  if(lightLevel > lightThreshold)
-  {
+  if(lightLevel > lightThreshold){
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     
@@ -48,8 +53,14 @@ void loop() {
     if(distance <= 200){
       tone(speakerPin, 2000);
       Serial.println(distance);
-    }else{
+    }else{ 
+      //if distance detected is too far
       noTone(speakerPin);
     }
+    
+  }else{
+    //if light level is low
+    noTone(speakerPin);
+    digitalWrite(trigPin, LOW);
   }
 }
---
+++ test/testSendMessageWithFona/testSendMessageWithFona.ino
@@ -0,0 +1,63 @@
+#include "Adafruit_FONA.h"
+
+#define FONA_RX 2
+#define FONA_TX 3
+#define FONA_RST 4
+
+#include <SoftwareSerial.h>
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+SoftwareSerial *fonaSerial = &fonaSS;
+
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+
+uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
+
+uint8_t type;
+
+void setup() {
+  // put your setup code here, to run once:
+  while(!Serial);
+  Serial.begin(115200);
+  Serial.println(F("FONA basic test"));
+  Serial.println(F("Initializing....(May take 3 seconds)"));
+
+    fonaSerial->begin(4800);
+  if (! fona.begin(*fonaSerial)) {
+    Serial.println(F("Couldn't find FONA"));
+    while (1);
+  }
+  type = fona.type();
+  Serial.println(F("FONA is OK"));
+  Serial.print(F("Found "));
+  switch (type) {
+    case FONA800L:
+      Serial.println(F("FONA 800L")); break;
+    case FONA800H:
+      Serial.println(F("FONA 800H")); break;
+    case FONA808_V1:
+      Serial.println(F("FONA 808 (v1)")); break;
+    case FONA808_V2:
+      Serial.println(F("FONA 808 (v2)")); break;
+    case FONA3G_A:
+      Serial.println(F("FONA 3G (American)")); break;
+    case FONA3G_E:
+      Serial.println(F("FONA 3G (European)")); break;
+    default: 
+      Serial.println(F("???")); break;
+  }
+  
+  // Print module IMEI number.
+  char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
+  uint8_t imeiLen = fona.getIMEI(imei);
+  if (imeiLen > 0) {
+    Serial.print("Module IMEI: "); Serial.println(imei);
+  }
+
+  char sendto[21] = "13478281699";
+  char message[141] = "Alert: Woodpeckers Detected";
+  fona.sendSMS(sendto, message);
+}
+
+
+void loop(){}
+

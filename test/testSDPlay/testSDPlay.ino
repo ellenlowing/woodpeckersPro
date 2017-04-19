cb07c1f46307a0c631a5fbad89b750a4e19da8b9
Ellen Lo <lowing@bu.edu>
2017-04-18 23:27:10 -0400

    Add test for playing sound file from sd

---
+++ test/testSDPlay/testSDPlay.ino
@@ -0,0 +1,20 @@
+#include "SD.h"
+#define SD_ChipSelectPin 4
+#include "TMRpcm.h"
+#include "SPI.h"
+
+TMRpcm tmrpcm;
+
+void setup(){
+  tmrpcm.speakerPin = 9;
+  Serial.begin(9600);
+  if (!SD.begin(SD_ChipSelectPin)) {
+    Serial.println("SD fail");
+  }
+
+  char file[] = "81.wav";
+  tmrpcm.setVolume(6);
+  tmrpcm.play(file);
+}
+
+void loop(){  }

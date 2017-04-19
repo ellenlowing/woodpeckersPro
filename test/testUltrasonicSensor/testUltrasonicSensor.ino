a54bfcc87c6e273459a4b86089d29f5686bd9850
Ellen Lo <lowing@bu.edu>
2017-04-18 23:01:37 -0400

    Added test for ultrasonic sensor

---
+++ test/testUltrasonicSensor/testUltrasonicSensor.ino
@@ -0,0 +1,27 @@
+const int trigPin = 9; //digital
+const int echoPin = 10; //digital
+
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    long duration = pulseIn(echoPin, HIGH);
+    
+    // Calculating the distance
+    int distance= duration*0.034/2;
+
+    Serial.println(distance);
+
+}

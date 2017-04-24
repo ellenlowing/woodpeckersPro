024e6d5346d51fc69868c977ccdeddd130e2cf82
Ellen Lo <lowing@bu.edu>
2017-04-24 18:51:23 -0400

    modified main

--- src/main/main.ino
+++ src/main/main.ino
@@ -12,11 +12,14 @@
 #define ECHO_PIN2     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define TRIGGER_PIN3  9  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN3     8  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define TRIGGER_PIN4  7  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN4     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonar(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
 NewPing sonar3(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE);
+NewPing sonar4(TRIGGER_PIN4, ECHO_PIN4, MAX_DISTANCE);
 
 #include <SoftwareSerial.h>
 SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
@@ -96,13 +99,16 @@ boolean birdDetected_new(){
   unsigned int uS_one = sonar.ping(); // Send ping, get ping time in microseconds (uS).
   unsigned int uS_two = sonar2.ping();
   unsigned int uS_three = sonar3.ping();
+  unsigned int uS_four = sonar4.ping();
   int distance1 = sonar.convert_cm(uS_one);
   int distance2 = sonar2.convert_cm(uS_two);
   int distance3 = sonar3.convert_cm(uS_three);
+  int distance4 = sonar4.convert_cm(uS_four);
   const int distanceThreshold = 30;
   boolean detected1 = false;
   boolean detected2 = false;
   boolean detected3 = false;
+  boolean detected4 = false;
   if(distance1 != 0 && distance1 <= distanceThreshold){
     detected1 = true;
   }
@@ -111,6 +117,9 @@ boolean birdDetected_new(){
   }
   if(distance3 != 0 && distance3 <= distanceThreshold){
     detected3 = true;
+  }
+  if(distance4 != 0 && distance4 <= distanceThreshold){
+    detected4 = true;
   }
   Serial.print("Ping1: ");
   Serial.print(distance1); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
@@ -121,7 +130,10 @@ boolean birdDetected_new(){
   Serial.print("Ping3: ");
   Serial.print(distance3); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
   Serial.println("cm");
-  return detected1 | detected2 | detected3;
+  Serial.print("Ping4: ");
+  Serial.print(distance4); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+  Serial.println("cm");
+  return detected1 | detected2 | detected3 | detected4;
   
  
 

5272a3e46ff14c40a31007521ee06c58a16c037e
Ellen Lo <lowing@bu.edu>
2017-04-25 15:31:51 -0400

    Add test for MP3 Shield

---
+++ test/testSpeaker/testSpeaker.ino
@@ -0,0 +1,136 @@
+#include <SFEMP3Shield.h>
+#include <SFEMP3ShieldConfig.h>
+#include <SFEMP3Shieldmainpage.h>
+
+/**
+ * \file MP3Shield_Library_Demo.ino
+ *
+ * \brief Example sketch of using the MP3Shield Arduino driver, demonstrating all methods and functions.
+ * \remarks comments are implemented with Doxygen Markdown format
+ *
+ * \author Bill Porter
+ * \author Michael P. Flaga
+ *
+ * This sketch listens for commands from a serial terminal (like the Serial
+ * Monitor in the Arduino IDE). If it sees 1-9 it will try to play an MP3 file
+ * named track00x.mp3 where x is a number from 1 to 9. For eaxmple, pressing
+ * 2 will play 'track002.mp3'. A lowe case 's' will stop playing the mp3.
+ * 'f' will play an MP3 by calling it by it's filename as opposed to a track
+ * number.
+ *
+ * Sketch assumes you have MP3 files with filenames like "track001.mp3",
+ * "track002.mp3", etc on an SD card loaded into the shield.
+ */
+
+#include <SPI.h>
+
+//Add the SdFat Libraries
+#include <SdFat.h>
+#include <SdFatUtil.h>
+
+//and the MP3 Shield Library
+#include <SFEMP3Shield.h>
+
+// Below is not needed if interrupt driven. Safe to remove if not using.
+#if defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_Timer1
+  #include <TimerOne.h>
+#elif defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_SimpleTimer
+  #include <SimpleTimer.h>
+#endif
+
+/**
+ * \brief Object instancing the SdFat library.
+ *
+ * principal object for handling all SdCard functions.
+ */
+SdFat sd;
+
+/**
+ * \brief Object instancing the SFEMP3Shield library.
+ *
+ * principal object for handling all the attributes, members and functions for the library.
+ */
+SFEMP3Shield MP3player;
+
+//------------------------------------------------------------------------------
+/**
+ * \brief Setup the Arduino Chip's feature for our use.
+ *
+ * After Arduino's kernel has booted initialize basic features for this
+ * application, such as Serial port and MP3player objects with .begin.
+ * Along with displaying the Help Menu.
+ *
+ * \note returned Error codes are typically passed up from MP3player.
+ * Whicn in turns creates and initializes the SdCard objects.
+ *
+ * \see
+ * \ref Error_Codes
+ */
+void setup() {
+
+  uint8_t result; //result code from some function as to be tested at later time.
+
+  Serial.begin(115200);
+
+  Serial.print(F("F_CPU = "));
+  Serial.println(F_CPU);
+  Serial.print(F("Free RAM = ")); // available in Version 1.0 F() bases the string to into Flash, to use less SRAM.
+  Serial.print(FreeRam(), DEC);  // FreeRam() is provided by SdFatUtil.h
+  Serial.println(F(" Should be a base line of 1028, on ATmega328 when using INTx"));
+
+
+  //Initialize the SdCard.
+  if(!sd.begin(SD_SEL, SPI_FULL_SPEED)) sd.initErrorHalt();
+  // depending upon your SdCard environment, SPI_HAVE_SPEED may work better.
+  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
+
+  //Initialize the MP3 Player Shield
+  result = MP3player.begin();
+  //check result, see readme for error codes.
+  if(result != 0) {
+    Serial.print(F("Error code: "));
+    Serial.print(result);
+    Serial.println(F(" when trying to start MP3 player"));
+    if( result == 6 ) {
+      Serial.println(F("Warning: patch file not found, skipping.")); // can be removed for space, if needed.
+      Serial.println(F("Use the \"d\" command to verify SdCard can be read")); // can be removed for space, if needed.
+    }
+  }
+
+#if defined(__BIOFEEDBACK_MEGA__) // or other reasons, of your choosing.
+  // Typically not used by most shields, hence commented out.
+  Serial.println(F("Applying ADMixer patch."));
+  if(MP3player.ADMixerLoad("admxster.053") == 0) {
+    Serial.println(F("Setting ADMixer Volume."));
+    MP3player.ADMixerVol(-3);
+  }
+#endif
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+    play();
+}
+
+void play(){
+    uint32_t offset = 0;
+    
+    uint8_t result;
+    //create a string with the filename
+    char trackName[] = "track001.mp3";
+
+#if USE_MULTIPLE_CARDS
+    sd.chvol(); // assign desired sdcard's volume.
+#endif
+    //tell the MP3 Shield to play that file
+    result = MP3player.playMP3(trackName, offset);
+    //check result, see readme for error codes.
+    if(result != 0) {
+      Serial.print(F("Error code: "));
+      Serial.print(result);
+      Serial.println(F(" when trying to play track"));
+    }   
+    delay(7000);
+}
